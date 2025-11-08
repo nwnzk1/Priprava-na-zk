a37e8bce5f0fe021beb08167a6136c5250d46df3
Vladan Korenek <vladan.korenek@unob.cz>
2025-11-08 13:07:08 +0100

    dedicnost + polymorfismus - ctení dat ze souboru

--- dedicnost_2_zk.cpp
+++ dedicnost_2_zk.cpp
@@ -1,6 +1,21 @@
-#include <iostream>
+﻿#include <iostream>
 #include <string>
 #include <vector>
+#include <fstream>
+#include <sstream>
+
+/* 
+
+vytvoření textové souboru zamestanci.txt s daty:
+
+P;Matej Novotny;20;42000.0;3;2000.0
+T;Matyas Kopecek;11;50450.0;8;580.0
+P;Marek Musil;21;40000;1;2000.0
+T;Matej Malek;10;48500.0;50;580.0 
+
+// můžeš si schválně změnit čísla, které jsou ve formátu int na čísla ve formátu double pro vyzkoušení try-catch 
+
+*/
 
 using namespace std;
 
@@ -21,7 +36,7 @@ public:
     virtual double vypocitejMzdu() = 0;
 
     virtual void vypisInfo() {
-        cout << "Jmeno: " << this->jmeno << "Identifikator: " << this->identifikator << "Zakladni plat: " << this->zakladniPlat << endl;
+        cout <<"Jmeno: " << this->jmeno << "Identifikator : " << this->identifikator << "Zakladni plat : " << this->zakladniPlat << endl;
     }
 
 };
@@ -46,7 +61,8 @@ public:
     }
 
     void vypisInfo() override {
-        cout << "Jmeno: " << this->jmeno << ", Identifikator: " << this->identifikator << ", Zakladni plat: " << this->zakladniPlat << ", Pocet projektu: " << this->pocetProjektu << endl;
+        cout << "Jmeno: " << this->jmeno << ", Identifikator: " << this->identifikator << ", Zakladni plat: " << 
+            this->zakladniPlat << ", Pocet projektu: " << this->pocetProjektu << endl;
     }
 };
 
@@ -70,25 +86,80 @@ public:
     }
 
     void vypisInfo() override {
-        cout << "Jmeno: " << this->jmeno << ", Identifikator: " << this->identifikator << ", Zakladni plat: " << this->zakladniPlat << ", Pocet chyb byl: " << this->pocetChyb << endl;
+        cout << "Jmeno: " << this->jmeno << ", Identifikator: " << this->identifikator << ", Zakladni plat: " << 
+            this->zakladniPlat << ", Pocet chyb byl: " << this->pocetChyb << endl;
     }
 };
 
 int main()
 {
-    Programator* zProgramator = new Programator("Matej Novotny", 22, 34500, 3, 2000);
-    Tester* zTester = new Tester("Matyas Kopecek", 34, 50450, 8, 580);
 
-    vector<Zamestnanec*> seznamZamestnancu;
-    seznamZamestnancu.push_back(zProgramator);
-    seznamZamestnancu.push_back(zTester);
+    vector<Zamestnanec*> seznamZamestancu;
 
-    for (Zamestnanec*& z_ptr : seznamZamestnancu) {
+
+    ifstream File("zamestanci.txt");
+    if (!File.is_open()) {
+        cerr << "Nepodarilo se otevrit soubor";
+        return 1;
+    }
+
+    string radek;
+
+
+    while (getline(File, radek)) {
+        stringstream ss(radek);
+        string pozice, jmeno, identifikator_str, zakladniPlat_str, pocetProjektu_str, bonusZaProjekt_str, pocetChyb_str, srazkaZaChybu_str;
+
+        getline(ss, pozice, ';');
+        getline(ss, jmeno, ';');
+        getline(ss, identifikator_str, ';');
+        getline(ss, zakladniPlat_str, ';');
+
+        // try-catch kvuli nalezení chyby, nemusí tam být (dávat si pozor na formát textového souboru - jestli všechny čísla sedí pro int/double/long kvuli stoi/stod/stol 
+        try {
+            int identifikator = stoi(identifikator_str);
+            double zakladniPlat = stod(zakladniPlat_str);
+
+            if (pozice == "P")
+            {
+                getline(ss, pocetProjektu_str, ';');
+                getline(ss, bonusZaProjekt_str, ';');
+                int pocetProjektu = stoi(pocetProjektu_str);
+                double bonusZaProjekt = stod(bonusZaProjekt_str);
+
+                seznamZamestancu.push_back(new Programator(jmeno, identifikator, zakladniPlat, pocetProjektu, bonusZaProjekt));
+            }
+            else if (pozice == "T")
+            {
+                getline(ss, pocetChyb_str, ';');
+                getline(ss, srazkaZaChybu_str, ';');
+                int pocetChyb = stoi(pocetChyb_str);
+                double srazkaZaChybu = stod(srazkaZaChybu_str);
+
+                seznamZamestancu.push_back(new Tester(jmeno, identifikator, zakladniPlat, pocetChyb, srazkaZaChybu));
+            }
+
+        } // Gemini generated kvůli abort() chybě
+        catch
+        (const std::invalid_argument& e) {
+            cerr << "CHYBA DAT (Konverze): Chyba formatu cisla na radku: " << radek << endl;
+        }
+        catch (const std::exception& e) {
+            cerr << "NEZNÁMÁ CHYBA na radku: " << radek << ". Detail: " << e.what() << endl;
+        }
+
+    }
+
+    File.close();
+
+    // vypis vektorů
+    for (Zamestnanec*& z_ptr : seznamZamestancu) {
         z_ptr->vypisInfo();
         z_ptr->vypocitejMzdu();
     }
 
-    for (Zamestnanec*& z_ptr : seznamZamestnancu) {
+    // delete vektorů
+    for (Zamestnanec*& z_ptr : seznamZamestancu) {
         delete z_ptr;
     }
 
