2a81b1204d30926b15417d57fe5f63e32dd7aee1
Marek Musil <marek.musil666@gmail.com>
2025-12-09 15:25:04 +0100

    Add files via upload

---
+++ Mara/delka vety.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <string>
+#include <fstream>
+
+
+
+int main()
+{
+	std::ifstream soubor("text_test.txt");
+	if(! soubor.is_open()) {
+		std::cout<<"Soubor se nepodarilo otevrit." << std::endl;
+		return 0;
+	}
+
+	char znak;
+	std::string celej_text;
+
+	while(soubor.get(znak)){
+		if(znak !=' ') {
+			celej_text += znak;
+		}
+	
+	}
+	
+	int delka = celej_text.length();
+	std::cout << "Delka textu je: " << delka << " znaku." << std::endl;
+}
+
